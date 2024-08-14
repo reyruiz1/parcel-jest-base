36184b052f01b9b7609fba2f0df52bcbc24e0a84
Reynaldo <reynaldo.ruiz@ucb.edu.bo>
2024-08-14 11:24:31 -0400

    Tarea4

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,11 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador"; // Importa la función multiplicar
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -12,4 +15,11 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
